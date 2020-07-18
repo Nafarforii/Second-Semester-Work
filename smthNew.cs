cdfe144d0cf96a7f8d197cec03ba1a509104c138
Filip Sokolov <lolipop6102@gmail.com>
2020-07-19 01:24:13 +0300

    test

---
+++ smthNew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemesterWork2
+{
+    class smthNew
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
